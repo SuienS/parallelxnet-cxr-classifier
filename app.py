7118972f3c73a55a1bba7643ccb91f3117fe1a64
SuienS <ravidu.rocks@gmail.com>
2020-12-08 11:38:12 +0530

    08_12_2020 - FINAL DRAFT PROT. - Code Commenting, added Logo ico and css.map and js.map

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
+"""
+=============================================================================
+Project     : Chest X-Ray Pathology Detection using Deep Learning
+Author Name : Rammuni Ravidu Suien Silva
+UoW No      : 16267097
+IIT No      : 2016134
+Module      : Final Year Project 20/21
+Supervisor  : Mr Pumudu Fernando
+
+Prototype   : Web Interface - BackEnd [Draft: .v01]
+University of Westminster, UK || IIT Sri Lanka
+=============================================================================
+"""
 import hashlib
 import json
-import sys
 import os
-import glob
-import re
-import numpy as np
 
-# Keras
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+import numpy as np
+from flask import Flask, request, render_template
+from flask import send_file
+from flask_jsglue import JSGlue
+# Tensorflow Keras imports
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from flask import send_file
-from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from flask_jsglue import JSGlue
 
+# Labels for classification tasks
 xray_labels = ["No Finding",
                "Enlarged Cardio.",
                "Cardiomegaly",
@@ -30,89 +41,106 @@ xray_labels = ["No Finding",
                "Fracture",
                "Support Devices"]
 
+# Flask Configs
+app = Flask(__name__)
+jsglue = JSGlue(app)
 
+
+# Detection function
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(320, 320))
 
-    # Preprocess
+    # Pre-processing of the input data
     x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
     x = np.expand_dims(x, axis=0)
 
-    # Prep
+    # Pre-processing image
     x = preprocess_input(x, mode='tf')
 
     preds = model.predict(x)
     return preds
 
 
+# Dummy function for the model
 def get_weighted_loss(x, y):
     return
 
 
-app = Flask(__name__)
-jsglue = JSGlue(app)
+# model load
 model = load_model('models/mimic_then_chex_model.h5', custom_objects={'weighted_loss': get_weighted_loss(1, 1)})
 cur_cxr_hash = 'none'
 
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
-
-
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        global cur_cxr_hash
-        # Get file from post request through the Web
-        cxr_img_file = request.files['file']
-
-        # Generating Hash
-        hashed_filename = hash_cxr(cxr_img_file)
-        print(hashed_filename)
-        cur_cxr_hash = hashed_filename
-        # Saving the CXR image to uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(hashed_filename))
-        cxr_img_file.save(file_path)
-
-        # Prediction calculation
-        preds = model_predict(file_path, model)
-
-        predictions_dict = {}
-        for i in range(0, len(xray_labels)):
-            predictions_dict[xray_labels[i]] = str(preds[0][i])
-
-        json_predictions = json.dumps(predictions_dict, indent=4)
-
-        # Creating detection result JSON to be sent
-        result = json_predictions  # Convert to string
-        return result
-    return None
-
-
-@app.route('/get_cxr_detect_img/<int:pathology_id>')
-def get_cxr_detect_img(pathology_id):
-    print(pathology_id)
-    global cur_cxr_hash
-    filepath = 'uploads/' + cur_cxr_hash
-    return send_file(filepath, mimetype='image/jpg')
-
-
+# Function for generating md5 of CXR image
 def hash_cxr(cxr_img):
     # Generating Hash
     cxr_md5_hash = hashlib.md5()
     cxr_md5_hash.update(cxr_img.read())
     cxr_hex_hash = cxr_md5_hash.hexdigest()
 
-    # Back to start of the file
-    cxr_img.seek(0)
+    cxr_img.seek(0)  # Back to start of the file
     hashed_filename = cxr_hex_hash + '.' + cxr_img.filename.split('.')[-1]
     return hashed_filename
 
 
-print("running...")
+"""
+==============================================================================
+                           Web request functions
+==============================================================================
+"""
+
+
+# Web page startup
+@app.route('/')
+def start_web():
+    return render_template("index.html")
+
+
+# CXR Image upload function
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        global cur_cxr_hash
+        # Getting image file from post request through the Web
+        cxr_img_file = request.files['file']
+
+        # Generating Hash of the image file
+        hashed_filename = hash_cxr(cxr_img_file)
+        print(hashed_filename)
+        cur_cxr_hash = hashed_filename
+
+        # Saving the CXR image to uploads
+        cxr_img_path = os.path.dirname(__file__)
+        file_path = os.path.join(
+            cxr_img_path, 'uploads', secure_filename(hashed_filename))
+        cxr_img_file.save(file_path)
+
+        # Detection results calculation
+        preds = model_predict(file_path, model)
+
+        # Creating the detection results dictionary/ JSON
+        predictions_dict = {}
+        for i in range(0, len(xray_labels)):
+            predictions_dict[xray_labels[i]] = str(preds[0][i])
+
+        # Creating detection result JSON to be sent
+        json_predictions = json.dumps(predictions_dict, indent=4)
+
+        result = json_predictions
+        return result
+    return None
+
+
+# Function for sending the localized CXR image
+@app.route('/get_cxr_detect_img/<int:pathology_id>')
+def get_cxr_detect_img(pathology_id):
+    print(pathology_id)
+    # use id to link to corresponding localization image TODO
+    global cur_cxr_hash
+    filepath = 'uploads/' + cur_cxr_hash
+    return send_file(filepath, mimetype='image/jpg')
+
+
+print("Server Running...")
 if __name__ == '__main__':
     app.run(debug=True)  # Debugging
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,22 @@
-var detResults = $('#result');
-var cxrPreview = $('#imagePreview');
-var popupCXRImg = $('#img-fluid');
+/*************************************************************************
+ * Project     : Chest X-Ray Pathology Detection using Deep Learning
+ * Author Name : Rammuni Ravidu Suien Silva
+ * UoW No      : 16267097
+ * IIT No      : 2016134
+ * Module      : Final Year Project 20/21
+ * Supervisor  : Mr Pumudu Fernando
 
+ * Prototype    : Web Interface - FrontEnd [Draft: .v01]
+ * University of Westminster, UK || IIT Sri Lanka
+ *************************************************************************/
+
+// consts for DOM objects
+const detResults = $('#result');
+const cxrPreview = $('#imagePreview');
+const popupCXRImg = $('#img-fluid');
+
+
+// Displaying the dynamic detection table
 function cxrResultsDisplayTable(dataJSON) {
     let tableHTML = '<div class="limiter">';
     tableHTML += '<div class="container-table100">';
@@ -15,18 +30,25 @@ function cxrResultsDisplayTable(dataJSON) {
     let keyList = Object.keys(dataJSON);
     let pathology_id = 0
 
+    // Dynamic table creation
     Object.keys(dataJSON).forEach(function (key) {
-        let forceRefresh = '?' + Math.floor(Math.random() * 10000)
-        let urlPath =  Flask.url_for('get_cxr_detect_img', {"pathology_id": pathology_id}) + forceRefresh;
-        $(cxrLocalizationPopup(pathology_id)).appendTo('#main-app-body');
+        let forceRefresh = '?' + Math.floor(Math.random() * 10000) // Force the browser to refresh image
+        let pathology = key;
+
+        // URL generation for the target image
+        //used library - http://stewartpark.github.io/Flask-JSGlue/ (PIP Installed)
+        let urlPath = Flask.url_for('get_cxr_detect_img', {"pathology_id": pathology_id}) + forceRefresh;
+        $(cxrLocalizationPopup(pathology_id, pathology)).appendTo('#main-app-body');
+
+        //Setting the image src
         let cxr_popup_img_id = "#cxrPopupImg-" + pathology_id;
         $(cxr_popup_img_id).attr('src', urlPath);
 
-        let pathology = key;
         let detectionRate = dataJSON[pathology];
-        if(parseFloat(detectionRate)>0.45){
+        // Highlighting high probable diseases
+        if (parseFloat(detectionRate) > 0.45) {
             tableHTML += '<div class="row bg-warning" data-toggle="modal" data-target="#cxrPopup-' + pathology_id + '">';
-        } else{
+        } else {
             tableHTML += '<div class="row" data-toggle="modal" data-target="#cxrPopup-' + pathology_id + '">';
         }
         tableHTML += '<div class="cell" data-title="Pathology">';
@@ -41,7 +63,6 @@ function cxrResultsDisplayTable(dataJSON) {
         pathology_id += 1;
     });
 
-
     tableHTML += '</div>';
     tableHTML += '</div>';
     tableHTML += '</div>';
@@ -49,15 +70,16 @@ function cxrResultsDisplayTable(dataJSON) {
     return tableHTML;
 }
 
-//used library - http://stewartpark.github.io/Flask-JSGlue/ (PIP Installed)
-function cxrLocalizationPopup(pathology_id) {
+// Dynamic popup card builder for localized image display
+function cxrLocalizationPopup(pathology_id, pathology) {
     let popupCXRHTML =
         '<div class="modal fade" id="cxrPopup-' + pathology_id + '" role="dialog">\n' +
         '        <div class="modal-dialog">\n' +
         '            <div class="card">\n' +
         '                <div class="card-img"><img id="cxrPopupImg-' + pathology_id + '" class="img-fluid"></div>\n' +
         '                <div class="card-text">\n' +
-        '                    <p>Localization Result</p>\n' +
+        '                    <p>Localization Result for:</p>\n' +
+        '                    <p><b>'+pathology+'</b></p>\n' +
         '                </div>\n' +
         '            </div>\n' +
         '        </div>\n' +
@@ -65,14 +87,14 @@ function cxrLocalizationPopup(pathology_id) {
     return popupCXRHTML
 }
 
-
+// Image upload front-end
 $(document).ready(function () {
-    // Init
+    // Initialization of page
     $('.image-section').hide();
     $('.loader').hide();
     detResults.hide();
 
-    // Upload Preview
+    // Uploaded CXR Preview
     function readURL(input) {
         if (input.files && input.files[0]) {
             let reader = new FileReader();
@@ -85,7 +107,8 @@ $(document).ready(function () {
         }
     }
 
-    $("#imageUpload").on("change",function () {
+    // Image upload button events and animations
+    $("#imageUpload").on("change", function () {
         $('.image-section').show();
         $('#btn-detect').show();
         detResults.text('');
@@ -93,15 +116,15 @@ $(document).ready(function () {
         readURL(this);
     });
 
-    // Predict
-    $('#btn-detect').on("click",function () {
+    // Detection results request
+    $('#btn-detect').on("click", function () {
         var form_data = new FormData($('#upload-file')[0]);
 
         // Show loading animation
         $(this).hide();
         $('.loader').show();
 
-        // Make prediction by calling api /predict
+        // Requesting the detection results by calling api /predict (ajax POST call)
         $.ajax({
             type: 'POST',
             url: '/predict',
@@ -115,8 +138,10 @@ $(document).ready(function () {
                 // Displaying detection results
                 $('.loader').hide();
                 detResults.fadeIn(600);
+
+                // Call for the creation of the detection results table
                 $(cxrResultsDisplayTable(data)).appendTo('#result');
-                console.log('Success!');
+                console.log('Detection DONE!');
             },
         });
     });
